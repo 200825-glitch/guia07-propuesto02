08a990b1cbb8f5c2f4ec6f974f0519975ff07e73
Andre Marcelo Mendoza Mayta <200825@unsaac.edu.pe>
2026-05-07 08:38:22 -0500

    Agregar campos y validacion para contactos

--- script.js
+++ script.js
@@ -3,10 +3,39 @@ function agregarContacto() {
     let nombre =
         document.getElementById("nombre").value;
 
+    let telefono =
+        document.getElementById("telefono").value;
+
+    let correo =
+        document.getElementById("correo").value;
+
+    let mensaje =
+        document.getElementById("mensaje");
+
+    if (
+        nombre === "" ||
+        telefono === "" ||
+        correo === ""
+    ) {
+        mensaje.textContent =
+            "Complete todos los campos";
+
+        return;
+    }
+
     let lista =
         document.getElementById("listaContactos");
 
     lista.innerHTML += `
-        <p>${nombre}</p>
+        <p>
+            ${nombre} - ${telefono} - ${correo}
+        </p>
     `;
+
+    mensaje.textContent =
+        "Contacto agregado correctamente";
+
+    document.getElementById("nombre").value = "";
+    document.getElementById("telefono").value = "";
+    document.getElementById("correo").value = "";
 }
